a113cec4e3eec2dd3f0dc230753c538a18c558f6
nottonofclay <tonofclay3@gmail.com>
2019-08-27 17:55:49 -0700

    Pikachu to First

--- Hello World/First.java
+++ Hello World/First.java
@@ -6,4 +6,10 @@ public class First {
 	public static void main(String[] args) {
 			System.out.println("Hello World!");
 	}
+	/*
+		System.out.println("Pikachu welcomes you to the world of pokemon");
+		System.out.println("\t\t\t  (\\_/)");
+		System.out.println("\t\t\t  (o^.^)");
+		System.out.println("\t\t\t(_(\")_(\")");
+	*/
 }
