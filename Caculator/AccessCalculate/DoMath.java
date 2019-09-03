190fa846ef721bcb401bfaac70cbb3770acb9405
nottonofclay <tonofclay3@gmail.com>
2019-09-03 09:48:17 -0700

    Edit calculate project

---
+++ Caculator/AccessCalculate/DoMath.java
@@ -0,0 +1,9 @@
+package AccessCalculate;
+import CalculateLibrary.Calculate;
+import java.util.Scanner;
+
+public class DoMath {
+    public static void main(String[] args) {
+    	Calculate.square(4);
+    }
+}
---
+++ Caculator/CalculateLibrary/Calculate.java
@@ -0,0 +1,8 @@
+package CalculateLibrary;
+import java.util.Scanner;
+
+public class Calculate {
+    public int square(int input){
+    	return input * input;
+    }
+}
